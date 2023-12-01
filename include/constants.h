31463de3d488a7b91db5d8254614da27c8d0d574
amirmat98 <matin.amirmahdi@gmail.com>
2023-12-01 01:59:48 +0100

    create constants

---
+++ include/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Shared memory key for key presses
+#define SHAREMEMORY_KEY_1 1234
+#define SEMAPHORE_KEY_1 "/my_semaphore_1"
+
+// Shared memory key for drone positions
+#define SHAREMEMORY_KEY_2 5678
+#define SEMAPHORE_KEY_2 "/my_semaphore_2"
+
+// Special value to indicate no key pressed
+#define NO_KEY_PRESSED 0
+
+#endif // CONSTANTS_H
